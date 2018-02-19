d08a4e0f38c22238527b556e6fbf7f585663a0be
ZongLUnli <mrzonglunli@gmail.com>
2018-02-19 16:00:49 +0800

    update user-login modules

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,7 @@ var config = {
         new webpack.HotModuleReplacementPlugin()
     ],
     devServer : {
-        hot                 :false,
+        hot: true,
         inline              : true, // 实时刷新
         port                : 8088
     }
