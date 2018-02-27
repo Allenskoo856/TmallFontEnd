fb07294e60d89812164263008ef486ce3747fdc9
李宗伦 <729390197@qq.com>
2018-02-27 16:10:57 +0800

    complete Cart module

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,56 +1,53 @@
-/*
-* @Author: Administrator
-* @Date:   2017-06-08 09:52:21
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-06-10 22:26:51
-*/
+
 
 'use strict';
 require('./index.css');
-var _mm = require('util/mm.js');
-var _user = require('service/user-service.js');
-
-//导航
-
+var _mm     = require('util/mm.js');
+var _user   = require('service/user-service.js');
+var _cart   = require('service/car-service.js');
+// 导航
 var nav = {
-    init: function () {
+    init : function(){
         this.bindEvent();
         this.loadUserInfo();
-        // this.loadCartCount();
+        this.loadCartCount();
+        return this;
     },
-    bindEvent: function () {
-        //登录点击事件
-        $('.js-login').click(function () {
+    bindEvent : function(){
+        // 登录点击事件
+        $('.js-login').click(function(){
             _mm.doLogin();
         });
-        //注册点击事件
-        $('.js-register').click(function () {
+        // 注册点击事件
+        $('.js-register').click(function(){
             window.location.href = './user-register.html';
         });
-        //退出点击事件
-        $('.js-logout').click(function () {
-            _user.logout(function (res) {
+        // 退出点击事件
+        $('.js-logout').click(function(){
+            _user.logout(function(res){
                 window.location.reload();
-            }, function (errMsg) {
-
+            }, function(errMsg){
+                _mm.errorTips(errMsg);
             });
         });
-
-
     },
-    //加载用户信息
-    loadUserInfo: function () {
-        _user.checkLogin(function (res) {
-            $('.user.not-login').hide();
-            $('.user.login').show().find('.username').text(res.username);
-
-        }, function (errMsg) {
-
+    // 加载用户信息
+    loadUserInfo : function(){
+        _user.checkLogin(function(res){
+            $('.user.not-login').hide().siblings('.user.login').show()
+                .find('.username').text(res.username);
+        }, function(errMsg){
+            // do nothing
         });
     },
-    loadCartCount: function () {
-
+    // 加载购物车数量
+    loadCartCount : function(){
+        _cart.getCartCount(function(res){
+            $('.nav .cart-count').text(res || 0);
+        }, function(errMsg){
+            $('.nav .cart-count').text(0);
+        });
     }
-
 };
+
 module.exports = nav.init();
--- src/page/user-login/index.js
+++ src/page/user-login/index.js
@@ -1,9 +1,3 @@
-/*
-* @Author: Rosen
-* @Date:   2017-05-08 22:26:19
-* @Last Modified by:   Rosen
-* @Last Modified time: 2017-05-21 22:36:14
-*/
 
 'use strict';
 require('./index.css');
